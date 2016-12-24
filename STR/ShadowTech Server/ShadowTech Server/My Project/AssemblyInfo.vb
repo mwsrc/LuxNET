11a9d99d28f1a2a678ac7f0e01ce9a5ccc181a90
Doge <lovely-doge@protonmail.com>
2016-12-24 16:54:09 +0900

    README

---
+++ STR/ShadowTech Server/ShadowTech Server/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("V3Lite Main UI Application")>
+<Assembly: AssemblyDescription("V3Lite Main UI Application")>
+<Assembly: AssemblyCompany("AhnLab, Inc")>
+<Assembly: AssemblyProduct("V3Lite")>
+<Assembly: AssemblyCopyright("Copyright (C) AhnLab, Inc. 1988-2016. All rights reserved.")>
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)> 
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("b51b89fd-27d1-4631-a8ab-848220782cdb")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("3.0.1.1")>
+<Assembly: AssemblyFileVersion("3.0.1.151")>
